d065f7e7b3ab305231df06d8a267aa745181285b
Rick-827 <henriquemelo97441@gmail.com>
2025-02-20 20:33:26 -0300

    aplicação da atv 4

--- aula2/src/variaveis/exercicio4.java
+++ aula2/src/variaveis/exercicio4.java
@@ -1,9 +1,28 @@
 package variaveis;
 
+import java.util.Scanner;
+
 public class exercicio4 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner jc = new Scanner(System.in);
+		
+		System.out.print("Digite o valor da capital inicial (R$): ");
+		double capital = jc.nextDouble();
+		
+		System.out.print("Digite a taxa de juros (% ao mes): ");
+		double taxa = jc.nextDouble() / 100;
+		
+		System.out.print("Digite o tempo (em meses): ");
+		int tempo = jc.nextInt();
+		
+		double juros = capital * taxa * tempo;
+		double montate = capital + juros;
+		
+		System.out.println("\nJuros acumulados: R$ " + juros);
+		System.out.println("Montate total: R$ " + montate);
+		
+		jc.close();
 
 	}
 
