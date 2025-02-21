bc5a4c7dc4e49b37dc0b211d7f1368ad7b140644
Rick-827 <henriquemelo97441@gmail.com>
2025-02-20 21:07:03 -0300

    att

--- aula2/src/variaveis/exercicio5.java
+++ aula2/src/variaveis/exercicio5.java
@@ -1,10 +1,27 @@
 package variaveis;
 
+import java.util.Scanner;
+
 public class exercicio5 {
-
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner dc = new Scanner(System.in);
+		
+		System.out.print("Digite o raio do circulo: ");
+		
+		double raio = dc.nextDouble();
+		
+		double area = Math.PI * Math.pow(raio, 2);
+		
+		double perimetro = 2 * Math.PI * raio;
+		
+		System.out.println("Área: " + area);
+		
+		System.out.println("Perímetro(circunferência): " + perimetro);
+		
+		dc.close();
+		
 	}
-
+	
 }
