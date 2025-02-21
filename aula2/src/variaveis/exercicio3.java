bfcbd9a9eb35fa0bf426820db4003531f56ae412
Rick-827 <henriquemelo97441@gmail.com>
2025-02-20 21:51:14 -0300

    Update exercicio3.java

--- aula2/src/variaveis/exercicio3.java
+++ aula2/src/variaveis/exercicio3.java
@@ -6,10 +6,10 @@ public class exercicio3 {
         Scanner jc = new Scanner(System.in);
         double soma = 0;
         
-        for (int i = 1; i <= 3; i++)
+        for (int i = 1; i <= 3; i++) {
             System.out.print("Pessoa " + i + ", digite um número: ");
             soma += jc.nextDouble();
-        
+        }
         
         System.out.println("A média é: " + (soma / 3));
         jc.close();
