22f42deb0154df75e71fb1330aba3bcccb17d6b7
Rick-827 <henriquemelo97441@gmail.com>
2025-02-26 08:23:20 -0300

    Update exercicio3.java

--- aula2/src/variaveis/exercicio3.java
+++ aula2/src/variaveis/exercicio3.java
@@ -1,17 +1,18 @@
+package variaveis;
 import java.util.Scanner;
 
-public class Exercicio3 {
+public class exercicio3 {
     public static void main(String[] args) {
     	
         Scanner jc = new Scanner(System.in);
         
-        System.out.println("Digite a 1º nota do aluno");
+        System.out.println("Digite a 1º nota do aluno: ");
         double n1 = jc.nextDouble();
 
-        System.out.println("Digite a 2º nota do aluno");
+        System.out.println("Digite a 2º nota do aluno: ");
         double n2 = jc.nextDouble();
 
-        System.out.println("Digite a 3º nota do aluno");
+        System.out.println("Digite a 3º nota do aluno: ");
         double n3 = jc.nextDouble();
 
         double media = (n1 + n2 + n3) / 3;
