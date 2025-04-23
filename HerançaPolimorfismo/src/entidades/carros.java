446160cf49ce51ef2a556179d112bd71f40f3cad
igornk777 <lucassrib2005@gmail.com>
2025-04-23 10:50:22 -0300

    att

--- HerançaPolimorfismo/src/entidades/carros.java
+++ HerançaPolimorfismo/src/entidades/carros.java
@@ -1,5 +1,22 @@
 package entidades;
 
-public class carros {
+public class carros extends veiculos{
+	
+	private boolean arCondicionado;
+
+	public carros(String marca, String modelo, int ano, double valorDiario, boolean arCondicionado) {
+		super(marca, modelo, ano, valorDiario);
+		
+		this.arCondicionado = arCondicionado;
+	}
+	//toString
+	public String toString() {
+		return "Marca: " + getmarca()+
+				"\nModelo: " + getmodelo() +
+				"\nAno: " + getano() +
+				"\nvalor Diaria: " + getvalorDiario() +
+				"\narCondicionado: " + arCondicionado;
+				
+	}
 
 }
--- HerançaPolimorfismo/src/entidades/motos.java
+++ HerançaPolimorfismo/src/entidades/motos.java
@@ -1,5 +1,21 @@
 package entidades;
 
-public class motos {
+public class motos extends veiculos {
+	
+	private int cilindradas;
+
+	public motos(String marca, String modelo, int ano, double valorDiario, int cilindradas) {
+		super(marca, modelo, ano, valorDiario);
+		
+		this.cilindradas = cilindradas;
+	}
+		//toString
+		public String toString() {
+			return "Marca: " + getmarca()+
+					"\nModelo: " + getmodelo() +
+					"\nAno: " + getano() +
+					"\nvalor Diaria: " + getvalorDiario() +
+					"\ncilindadras: " + cilindradas;
+	}
 
 }
